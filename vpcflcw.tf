dce54266486f183df11eb88ca112926d10898da7
lilib.kurian <lilib.kurian@gmail.com>
2020-05-09 09:33:40 -0400

    update code

--- vpcflcw.tf
+++ vpcflcw.tf
@@ -1,11 +1,3 @@
-provider "aws" {
-  #alias = "dev"
-
-  region     = "us-east-1"
-  access_key = "AKIAXBLXXOWWF5I7OK6Q"
-  secret_key = "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"
-}
-
 resource "aws_flow_log" "vpcflowlog-cwl" {
   #provider = "${var.vfl_cw_provider}"
 
