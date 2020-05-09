ff0876334d7bcb884ddb66a36b44497dfa0c1661
lilib.kurian <lilib.kurian@gmail.com>
2020-05-09 09:35:23 -0400

    update var

--- vpcflcw.tf
+++ vpcflcw.tf
@@ -1,9 +1,9 @@
 resource "aws_flow_log" "vpcflowlog-cwl" {
   #provider = "${var.vfl_cw_provider}"
 
-  iam_role_arn    = "${var.vfl_cw_iam_role_arn}"
-  log_destination = "${var.vfl_cw_log_destination}"
-  traffic_type    = "${var.vfl_cw_traffic_type}"
-  vpc_id          = "${var.vfl_cw_vpc_id}"
+  iam_role_arn    = var.vfl_cw_iam_role_arn
+  log_destination = var.vfl_cw_log_destination
+  traffic_type    = var.vfl_cw_traffic_type
+  vpc_id          = var.vfl_cw_vpc_id
 
 }
